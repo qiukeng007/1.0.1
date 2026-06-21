9fbacaca54733c06183904be9dcfe4495743944e
qiukeng007 <qiukeng007@163.com>
2026-06-21 21:30:10 +0200

    iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -25,7 +25,6 @@ import WebKit
       }
     }
 
-    GeneratedPluginRegistrant.register(with: self.pluginRegistry)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
